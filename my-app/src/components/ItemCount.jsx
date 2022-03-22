9cb09cc5017c417877a3b8ee568acd1cb09b7470
Gyrkora <giselle.gallardo0210@gmail.com>
2022-03-21 23:13:46 -0300

    Desafio CountItem

--- my-app/src/components/ItemCount.jsx
+++ my-app/src/components/ItemCount.jsx
@@ -4,8 +4,13 @@ import { Button } from "./Button";
 
 export const ItemCount = () => {
   const start = 0;
+  const stock = 10;
 
   const [count, setCount] = useState(start);
+  const noLess =
+    count === start ? (Button.disabled = true) : (Button.disabled = false);
+
+    const noMore = count === stock ? (Button.disabled = true) : (Button.disabled = false);
 
   const addProduct = (num) => {
     setCount(count + num);
@@ -14,18 +19,11 @@ export const ItemCount = () => {
   return (
     <div>
       <ItemCountContainer>
-        <button
-          onClick={() => addProduct(-1)}
-          disabled={
-            count === start
-              ? (Button.disabled = true)
-              : (Button.disabled = false)
-          }
-        >
+        <button onClick={() => addProduct(-1)} disabled={noLess}>
           -
         </button>
         <h3>{count}</h3>
-        <button onClick={() => addProduct(+1)}>+</button>
+        <button onClick={() => addProduct(+1)} disabled={ noMore }>+</button>
       </ItemCountContainer>
 
       <div>
