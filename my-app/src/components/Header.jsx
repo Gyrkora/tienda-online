54427c9de979bad26ad6266109f4f970971f90f9
Gyrkora <giselle.gallardo0210@gmail.com>
2022-03-30 23:39:29 -0300

    PreEntrega

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -4,6 +4,7 @@ export const Header = () => {
   return (
     <div>
       <header className="App-header">
+        <h1> Este es el Inicio </h1>
         <img src={tow} className="App-logo" alt="logo" />
       </header>
     </div>
