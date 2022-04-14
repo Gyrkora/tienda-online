6185fbc7d26f8b2143e1431a5ad8f0eb11925fa5
Gyrkora <giselle.gallardo0210@gmail.com>
2022-04-14 00:46:44 -0300

    changes

--- my-app/src/components/Cart.jsx
+++ my-app/src/components/Cart.jsx
@@ -4,6 +4,9 @@ import { useCartContext } from "../context/CartContext";
 export const Cart = () => {
   const { cartList, removeCart, removeByItem } = useCartContext();
 
+  // const totalPrice = cartList.reduce((a, c) => a + c.amount * c.priceInDollars, 0);
+
+
   return (
     <div>
       {cartList.map((item) => {
@@ -13,7 +16,7 @@ export const Cart = () => {
               <h1> {item.courseName} </h1>
               <img src={item.photo} alt={item.courseName} />
               <h2> Cantidad: {item.amount} </h2>
-
+              <h4>{(item.amount * item.priceInDollars) + ' USD'}</h4>
               <button onClick={() => removeByItem(item.id)}>&#10005;</button>
               
             </div>
@@ -22,7 +25,11 @@ export const Cart = () => {
       })}
 
       {cartList.length ? (
-        <button onClick={removeCart}>Vaciar Carrito</button>
+        <div>
+          <button onClick={removeCart}>Vaciar Carrito</button>
+          {/* <h3>{totalPrice}</h3> */}
+
+        </div>
       ) : (
         <div>
           <h3>No hay productos para mostrar</h3>
